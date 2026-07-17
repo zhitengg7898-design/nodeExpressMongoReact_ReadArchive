ba2e90ddaa301c703324ea75fe09e41a97758209
zhitguo <guo.zhit@northeastern.edu>
2026-07-16 20:34:10 -0400

    delete models

--- models/Book.js
+++
@@ -1,3 +0,0 @@
-import { getDB } from "../config/db.js";
-
-export const books = () => getDB().collection("books");
--- models/User.js
+++
@@ -1,3 +0,0 @@
-import { getDB } from "../config/db.js";
-
-export const users = () => getDB().collection("users");
